a567be7c06efe80a4f0177ed5b4ada0ca30ed2f1
qianchu <hey_flora@126.com>
2020-01-30 17:31:09 +0000

    pipeline test

--- supervised_wsd.sh
+++ supervised_wsd.sh
@@ -4,10 +4,16 @@ model=$1
 cuda=$2
 lg=$3
 
+if [ $cuda -lt 0 ]
+then
+cuda_flag=0
+else
+cuda_flag=1
+fi
 
 for dict in ../../training/data/$model_100000_cluster_wsd0_cwn_trans_wsd.clustered.0.en.vec_dict_wps.mono.multiwsd_poly*
 do
-CUDA_VISIBLE_DEVICES=$cuda python supervised.py --src_lang en --tgt_lang $lg --src_emb ../../training_data/$model_100000_cluster_wsd_random0_cwn_trans_wsd.clustered.0.en.vec.clean --tgt_emb ../../training_data/$model_100000_cluster_wsd_random0_cwn_trans_wsd.clustered.0.$lg.vec.clean --n_refinement 60 --emb_dim 768 --dico_train $dict --cuda 1 --dico_eval ./data/crosslingual/dictionaries/en-zh.txt.unsup &> supervised_clusterwsdrandom_$(basename $dict).log
-CUDA_VISIBLE_DEVICES=$cuda python supervised.py --src_lang en --tgt_lang $lg --src_emb ../../training_data/$model_100000_cluster_wsd0_cwn_trans_wsd.clustered.0.en.vec.clean --tgt_emb ../../training_data/$model_100000_cluster_wsd0_cwn_trans_wsd.clustered.0.$lg.vec.clean --n_refinement 60 --emb_dim 768 --dico_train $dict --cuda 1 --dico_eval ./data/crosslingual/dictionaries/en-zh.txt.unsup &> supervised_clusterwsd_$(basename $dict).log
+CUDA_VISIBLE_DEVICES=$cuda python supervised.py --src_lang en --tgt_lang $lg --src_emb ../../training_data/$model_100000_cluster_wsd_random0_cwn_trans_wsd.clustered.0.en.vec.clean --tgt_emb ../../training_data/$model_100000_cluster_wsd_random0_cwn_trans_wsd.clustered.0.$lg.vec.clean --n_refinement 60 --emb_dim 768 --dico_train $dict --cuda $cuda_flag --dico_eval ./data/crosslingual/dictionaries/en-zh.txt.unsup &> supervised_clusterwsdrandom_$(basename $dict).log
+CUDA_VISIBLE_DEVICES=$cuda python supervised.py --src_lang en --tgt_lang $lg --src_emb ../../training_data/$model_100000_cluster_wsd0_cwn_trans_wsd.clustered.0.en.vec.clean --tgt_emb ../../training_data/$model_100000_cluster_wsd0_cwn_trans_wsd.clustered.0.$lg.vec.clean --n_refinement 60 --emb_dim 768 --dico_train $dict --cuda $cuda_flag --dico_eval ./data/crosslingual/dictionaries/en-zh.txt.unsup &> supervised_clusterwsd_$(basename $dict).log
 
 done
