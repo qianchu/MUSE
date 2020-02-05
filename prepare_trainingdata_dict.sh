35e4cca4bc473e73fc4e7e3b68b4f5d75133ec3b
qianchu <hey_flora@126.com>
2020-02-05 13:59:20 +0000

    pipeline dict

--- prepare_trainingdata_dict.sh
+++ prepare_trainingdata_dict.sh
@@ -3,13 +3,14 @@
 en_emb=$1
 zh_emb=$2
 test_dict=$3
-dict_sizes=$3
-polys=$4
+dict_sizes=$4
+polys=$5
 
 for dict_size in $dict_sizes
 do
 for poly in $polys
 do
+echo "python prepare_trainingdata_dict.py --emb_en .$en_emb --emb_zh $zh_emb --dict_wsd_produce --dict_size $dict_size --poly_percent $poly --dict_test $test_dict"
 python prepare_trainingdata_dict.py --emb_en .$en_emb --emb_zh $zh_emb --dict_wsd_produce --dict_size $dict_size --poly_percent $poly --dict_test $test_dict
 done
 done
