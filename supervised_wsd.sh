554f37961bab48a6ce8075a306a625028d56ac12
qianchu <hey_flora@126.com>
2020-02-05 15:11:41 +0000

    dict flag for clusterall (supervised_wsd.sh)

--- supervised_wsd.sh
+++ supervised_wsd.sh
@@ -16,12 +16,23 @@ fi
 
 #declare -a cluster_flags=(cluster_flags)
 
-
-for dict in ../../training_data/${model}_100000_cluster_wsd0_cwn_trans_wsd.clustered.0.en.vec_dict_wps.mono.multiwsd_poly*_${dict_size}_*
-do
-
 for cluster_flag in "$cluster_flags"
 do
+
+if [ "$cluster_flag" == "clusterall" ]
+then
+dict_flag='nowsd'
+else
+dict_flag='wsd'
+fi
+
+for dict in ../../training_data/${model}_100000_cluster_wsd0_cwn_trans_wsd.clustered.0.en.vec_dict_wps.mono.multi${dict_flag}_poly*_${dict_size}_*
+do
+
+
+
+
+
 echo "CUDA_VISIBLE_DEVICES=$cuda python supervised.py --src_lang en --tgt_lang $lg --src_emb ../../training_data/${model}_100000_${cluster_flag}0_cwn_trans_wsd.clustered.0.en.vec.clean --tgt_emb ../../training_data/${model}_100000_${cluster_flag}0_cwn_trans_wsd.clustered.0.$lg.vec.clean --n_refinement 60 --emb_dim 768 --dico_train $dict --cuda $cuda_flag --dico_eval ./data/crosslingual/dictionaries/en-zh.txt.unsup &> supervised_${cluster_flag}_$(basename $dict).log"
 CUDA_VISIBLE_DEVICES=$cuda python supervised.py --src_lang en --tgt_lang $lg --src_emb ../../training_data/${model}_100000_${cluster_flag}0_cwn_trans_wsd.clustered.0.en.vec.clean --tgt_emb ../../training_data/${model}_100000_${cluster_flag}0_cwn_trans_wsd.clustered.0.$lg.vec.clean --n_refinement 60 --emb_dim 768 --dico_train $dict --cuda $cuda_flag --dico_eval ./data/crosslingual/dictionaries/en-zh.txt.unsup &> supervised_${cluster_flag}_$(basename $dict).log
 
